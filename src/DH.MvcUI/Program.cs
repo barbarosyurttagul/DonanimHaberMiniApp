91606af170bd5fccfaeeca9cd6798708f4f51485
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2022-09-01 19:19:28 +0300

    Dependency Injections added

--- src/DH.MvcUI/Program.cs
+++ src/DH.MvcUI/Program.cs
@@ -1,7 +1,16 @@
+using DH.Business.Abstract;
+using DH.Business.Concrete;
+using DH.DataAccess.Abstract;
+using DH.DataAccess.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Dependency Injections
+builder.Services.AddScoped<IPostService, PostManager>();
+builder.Services.AddScoped<IPostDal, AdoPostDal>();
 
 var app = builder.Build();
 
@@ -10,10 +19,10 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
