82e0c7bb6de7abbeead83d64152c9df07e5f91ec
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2022-09-01 18:38:40 +0300

    HomeController-Index updated and PostIndexviewModel

--- src/DH.MvcUI/Controllers/HomeController.cs
+++ src/DH.MvcUI/Controllers/HomeController.cs
@@ -1,21 +1,32 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using DH.MvcUI.Models;
+using DH.Business.Abstract;
+using X.PagedList;
 
 namespace DH.MvcUI.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IPostService _postService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IPostService postService)
     {
         _logger = logger;
+        _postService = postService;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(int? page)
     {
-        return View();
+        var pageNumber = page ?? 1;
+            var pageSize = 3;
+            var posts = _postService.GetAll().ToPagedList(pageNumber, pageSize);
+            var model = new PostIndexViewModel
+            {
+                posts = posts
+            };
+            return View(model);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ src/DH.MvcUI/Models/PostIndexViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DH.Entities.Concrete;
+
+namespace DH.MvcUI.Models
+{
+    public class PostIndexViewModel
+    {
+        public List<Post> posts { get; set; }
+    }
+}
